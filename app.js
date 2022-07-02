a5c25ec9ccfb891a646ba0828a8c5ceb96a38c0c
sanjay-personal <sanjay.gajjala1995@gmail.com>
2022-07-02 12:18:13 +0530

    added commit

--- app.js
+++ app.js
@@ -32,6 +32,8 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+// app.use(express.static(__dirname + '/dist/'));
+
 
 // con.database();
 
